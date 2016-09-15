b4fd7a2cd2cf76f0b20306c45e84fc99a3288c45
Chris Anthony <CTAnthny@gmail.com>
2016-09-15 04:32:17 -0400

    Added movie list

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "pg"
+require 'pry'
 
 set :views, File.join(File.dirname(__FILE__), "app/views")
 
@@ -35,4 +36,24 @@ end
 get '/actors/:actor_name' do
   @actor_name = params[:actor_name]
   erb :show_actor
+end
+
+get '/movies' do
+  @movies = db_connection do |conn|
+    get_movies_query = %(
+      SELECT movies.id, movies.title AS movie, movies.year, movies.rating,
+        genres.name AS genre, studios.name AS studio
+      FROM movies
+        JOIN genres ON movies.genre_id = genres.id
+        RIGHT OUTER JOIN studios ON movies.studio_id = studios.id
+      ORDER BY movies.title ASC
+    )
+    conn.exec(get_movies_query)
+  end
+  erb :movies
+end
+
+get '/movies/:id' do
+  @movie_id = params[:id]
+  erb :show_movie
 end
