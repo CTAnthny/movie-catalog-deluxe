aa4e6742dd91f2a063fac736cdfad09ad0d1e395
Chris Anthony <CTAnthny@gmail.com>
2016-09-17 03:40:39 -0400

    Added individual movie functionality

--- server.rb
+++ server.rb
@@ -55,5 +55,38 @@ end
 
 get '/movies/:id' do
   @movie_id = params[:id]
+  movie_id_a = []
+  movie_id_a << @movie_id.to_i
+  db_connection do |conn|
+    get_movie_info_query = %(
+      SELECT movies.title, movies.year, movies.synopsis, movies.rating,
+        genres.name AS genre, studios.name AS studio
+      FROM movies
+        JOIN genres ON movies.genre_id = genres.id
+        RIGHT OUTER JOIN studios ON movies.studio_id = studios.id
+      WHERE movies.id = $1
+    )
+    @movie_info = conn.exec_params(get_movie_info_query, movie_id_a)
+
+    get_actor_info_query = %(
+      SELECT cast_members.character AS characters, actors.name AS actors
+      FROM cast_members
+        JOIN actors ON cast_members.actor_id = actors.id
+      WHERE cast_members.movie_id = $1
+    )
+    @actor_info = conn.exec(get_actor_info_query, movie_id_a)
+  end
   erb :show_movie
 end
+
+# SELECT movies.title, movies.year, movies.synopsis, movies.rating,
+#   genres.name AS genre, studios.name AS studio,
+# FROM movies
+#   JOIN genres ON movies.genre_id = genres.id
+#   RIGHT OUTER JOIN studios ON movies.studio_id = studios.id
+# WHERE movies.id = @movie_id
+#
+# SELECT cast_members.character, actors.name
+# FROM cast_members
+#   JOIN actors ON cast_members.actor_id = actors.id
+# WHERE cast_members.movie_id = @movie_id
